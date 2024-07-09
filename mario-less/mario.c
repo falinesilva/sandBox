18ba9fb9c464026a0591f41bb8d4bc6e74c22797
custodf <faline.custodio.da.silva@gmail.com>
2024-07-09 19:27:46 +0000

    Tue, Jul 9, 2024, 4:27 PM -03:00

---
+++ mario-less/mario.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "cs50.h"
+
+void print_row(int space, int hash);
+
+int main(void)
+
+{
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        print_row(n - i, i);
+    }
+
+}
+
+void print_row(int space, int hash)
+{
+    for (int i = 1; i < space; i++)
+    {
+        printf(" ");
+    }
+    for (int j = 0; j <= hash; j++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
