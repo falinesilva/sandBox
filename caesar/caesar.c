d8f993b5e96ef7e02156ce6af0b1d1f4f28e0d16
custodf <faline.custodio.da.silva@gmail.com>
2024-08-22 16:08:10 +0000

    Thu, Aug 22, 2024, 1:08 PM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,19 +1,19 @@
-#include <ctype.h>
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 // Verify key is a single command.
-bool check_key (int argc, string arg);
+bool check_key(int argc, string arg);
 
 // Encipher a plain text input with the key.
-string encipher (string plaintext, int key);
+string encipher(string plaintext, int key);
 
 int main(int argc, string argv[])
 {
     // Get key and verify.
-    if (!check_key (argc, argv[1]))
+    if (!check_key(argc, argv[1]))
     {
         printf("Usage: ./caesar key");
         return 1;
@@ -32,13 +32,13 @@ int main(int argc, string argv[])
     printf("ciphertext: %s\n", plain);
 }
 
-string encipher (string plaintext, int secret)
+string encipher(string plaintext, int secret)
 {
     int n = strlen(plaintext);
     for (int i = 0; i < n; i++)
     {
         // Encrypt alphabetical characters using the Caesar cipher formula.
-       if (isalpha(plaintext[i]))
+        if (isalpha(plaintext[i]))
         {
             if (islower(plaintext[i]))
             {
@@ -53,15 +53,17 @@ string encipher (string plaintext, int secret)
     return plaintext;
 }
 
-bool check_key (int argc, string arg)
+bool check_key(int argc, string arg)
 {
     // Verify key is a single argument.
-    if (argc != 2) return false;
+    if (argc != 2)
+        return false;
 
     // Verify key is a valid non-negative integer.
     for (int i = 0; arg[i] != '\0'; i++)
     {
-        if (!isdigit(arg[i])) return false;
+        if (!isdigit(arg[i]))
+            return false;
     }
     return true;
 }
