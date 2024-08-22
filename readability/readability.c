753937d11478275ccc3c7183fcaf5add46cb53f1
custodf <faline.custodio.da.silva@gmail.com>
2024-08-22 16:45:33 +0000

    Thu, Aug 22, 2024, 1:45 PM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,19 +1,19 @@
-#include <ctype.h>
 #include <cs50.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <ctype.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // Calculate grade level of user's text.
-int grade_calc (string text);
+int grade_calc(string text);
 // Constant for error message.
 int main(void)
 {
     int grade = 0;
 
     // Ask user to input text.
-    string text = get_string("\nCalculate the approximate grade level of some text.\n\n**********************************************\nType text and press Enter: \n");
+    string text = get_string("Type text and press Enter: \n");
     // Calculate grade level of the text.
     grade = grade_calc(text);
     grade = round(grade);
@@ -33,32 +33,32 @@ int main(void)
     }
 }
 
-int grade_calc (string text)
+int grade_calc(string text)
 {
     int letters = 0;
     int words = 1;
     int sentences = 0;
 
-// Calculate the amount of letters, words and senteces in the text.
+    // Calculate the amount of letters, words and senteces in the text.
     for (int i = 0; i < strlen(text); i++)
     {
         if (isalpha(text[i]))
         {
-            letters ++;
+            letters++;
         }
         else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
         {
-            sentences ++;
+            sentences++;
         }
         else if (isblank(text[i]))
         {
-            words ++;
+            words++;
         }
     }
 
-    //Calculate averages.
-    float average_letters = (float)letters / words *100;
-    float average_sentences = (float)sentences / words *100;
+    // Calculate averages.
+    float average_letters = (float) letters / words * 100;
+    float average_sentences = (float) sentences / words * 100;
 
     // Calculate grade based on Coleman-Liau index.
     float result = 0.0588 * average_letters - 0.296 * average_sentences - 15.8 + 0.5;
