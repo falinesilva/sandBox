8f133c15a6edd31d41afbd8a530b9006713d7392
custodf <faline.custodio.da.silva@gmail.com>
2024-10-15 22:26:06 +0000

    Tue, Oct 15, 2024, 7:26 PM -03:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -65,13 +65,13 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    for (int i = 0; i < candidate_count ; i ++)
+    for (int i = 0; i < candidate_count; i++)
     {
         if (strcmp(candidates[i].name, name) == 0)
-            {
-                candidates[i].votes ++;
-                return true;
-            }
+        {
+            candidates[i].votes++;
+            return true;
+        }
     }
     return false;
 }
