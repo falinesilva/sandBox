546a41b5ebd9f5866c89a7d1605910174387689c
custodf <faline.custodio.da.silva@gmail.com>
2024-10-21 17:46:33 +0000

    Mon, Oct 21, 2024, 2:46 PM -03:00

---
+++ deep/deep.py
@@ -0,0 +1,13 @@
+# Ask user for the answer
+
+answer = input ("What is the Answer to the Great Question of Life, the Universe and Everything? ")
+
+# If answer is correct, print Yes
+
+if answer == "42" or answer == "forty-two" or answer == "forty two":
+    print ("Yes")
+
+# If answer is incorrect, print No
+
+else:
+    print ("No")
