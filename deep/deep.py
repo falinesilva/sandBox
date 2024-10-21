c29a2f77b360f8a4255822c2f554e714624127be
custodf <faline.custodio.da.silva@gmail.com>
2024-10-21 17:53:31 +0000

    Mon, Oct 21, 2024, 2:53 PM -03:00

--- deep/deep.py
+++ deep/deep.py
@@ -2,12 +2,13 @@
 
 answer = input ("What is the Answer to the Great Question of Life, the Universe and Everything? ")
 
-# If answer is correct, print Yes
+# Remove spaces in front and back and make all lower case
+answer = answer.lower().strip()
 
+# If answer is correct, print Yes
 if answer == "42" or answer == "forty-two" or answer == "forty two":
     print ("Yes")
 
 # If answer is incorrect, print No
-
 else:
     print ("No")
