3c365ef1cabded833b3fabb4c3e9d9ee2c8404a0
custodf <faline.custodio.da.silva@gmail.com>
2024-10-21 23:27:53 +0000

    Mon, Oct 21, 2024, 8:27 PM -03:00

--- meal/meal.py
+++ meal/meal.py
@@ -2,7 +2,7 @@ def main():
 # Ask user what time it is
     time = input ("What time is it? ")
 
-    time_converted = float (convert(time))
+    time_converted = float (convert(time), end="")
 
     if time_converted == 7 <= 8.0:
         print ("breakfast time")
