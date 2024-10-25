bafa10faa28886e7d6930313485d9571cfd0973f
custodf <faline.custodio.da.silva@gmail.com>
2024-10-25 16:29:47 +0000

    Fri, Oct 25, 2024, 1:29 PM -03:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,13 +1,16 @@
 due = int (50)
 change = int (0)
+paid = int (0)
+coin = int (0)
 print ("Amount Due: ",due)
-coins = int (input ("Insert Coin: "))
 
-while coins < 50:
-    print ("Amount Due: ",due - coins)
-    coins += int (input ("Insert Coin: "))
+while paid < 50:
+    coin = int (input ("Insert Coin: "))
+    if coin in [25, 10, 5]:
+        paid += coin
+        print ("Amount Due:",due - paid)
 
-if coins > 50:
-    change = coins - due
+if paid > 50:
+    change = paid - due
 
-print ("Change Due: ",change)
+print ("Change Due:",change)
