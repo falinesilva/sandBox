936b1653fae77cfb6a9784e821d7de16cfb288c7
custodf <faline.custodio.da.silva@gmail.com>
2024-11-01 14:50:21 +0000

    Fri, Nov 1, 2024, 11:50 AM -03:00

--- plates/plates.py
+++ plates/plates.py
@@ -14,27 +14,14 @@ def is_valid(s):
     if len(s) > 6 or len(s) < 2:
         return False
 
-    # Check if first two characters are alphabetical
-    if not s[0].isalpha() and s[1].isalpha():
-        return False
-
-    # Check numbers in plate
-    for i, c in enumerate(s):
-        if c.isdigit():
-            # Found the first digit
-            if c == "0":
-                return False
-            # Check if all remaining characters are digits
-            elif all(char.isdigit() for char in s[i:]):
-                break
-            else:
-                return False
-
-
     # Check that all characters are alphanumeric
     for i in s:
         if not i.isalnum():
             return False
+
+    # Check if first two characters are alphabetical
+    if not s[0:1].isalpha():
+        return False
 
     # Check if first number is 0
     if s[2] == "0":
@@ -43,6 +30,18 @@ def is_valid(s):
     for c in s[:2]:
         if c.isdigit() and s[4].salpha:
             return False
+
+    # Check if all numbers come at the end.
+    if s[-1].isalpha() and not s[-2].isalpha():
+        return False
+
+    if s[-3].isalpha() and s[-4].isdigit():
+         return False
+
+    x = len(s) // 2
+
+    if s[x].isdigit():
+        return False
 
     else:
         return True
