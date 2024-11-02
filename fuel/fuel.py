30e3f405805759a245f340fcd7720027df5b91a3
custodf <faline.custodio.da.silva@gmail.com>
2024-11-02 05:01:59 +0000

    Sat, Nov 2, 2024, 2:01 AM -03:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -3,14 +3,11 @@ def main():
         try:
             fraction = input("Fraction: ")
             x, y = map(int, fraction.split('/'))
-            # Check if y is zero to avoid division by zero
-            x = round(x)
-            y = round(y)
             if y == 0:
                 raise ValueError("Denominator cannot be zero.")
-            # Calculate the percentage
             percentage = (x / y) * 100
-            # Display 'E' for empty (1% or less) and 'F' for full (99% or more)
+            percentage = round(percentage)
+
             if percentage <= 1:
                 print("E")
             elif percentage >= 99:
