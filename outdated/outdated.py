101f9e95a302b741bd613ab74152dce9fabb9c26
custodf <faline.custodio.da.silva@gmail.com>
2024-11-11 19:19:19 +0000

    Mon, Nov 11, 2024, 4:19 PM -03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -19,31 +19,27 @@ months_list = [
 
 while True:
     try:
-        user_date = "" # Reset input each loop
         user_date = input("Date: ")
         if "/" in user_date: # Check for / in input
             slash_date = user_date.split("/") # Split string to list
-            month = slash_date[0]
+            month = int (slash_date[0])
             if month > 12:
                 raise TypeError
-            day = slash_date [1]
+            day = int (slash_date [1])
             if day > 31:
                 raise TypeError
             year = slash_date[2]
-            print (year, month.zfill(2), day.zfill(2), sep ="-") # Print formatted date with padding
-            exit()
+            print (year, str(month).zfill(2), str(day).zfill(2), sep ="-") # Print formatted date with padding
         elif "," in user_date: # Check for , in input
             comma_date = user_date.split()
             year = comma_date[2]
-            day = comma_date [1].replace(',', '').replace('st', '').replace('rd', '').replace('nd', '').replace('th', '')
+            day = int (comma_date [1].replace(',', '').replace('st', '').replace('rd', '').replace('nd', '').replace('th', ''))
             if day > 31:
                 raise TypeError
-            month = comma_date[0]
-            if month > 12:
-                raise TypeError
             month_numbered = str (months_list.index(comma_date[0]) + 1)
-            print (year, month_numbered.zfill(2), day.zfill(2), sep = "-")
-            exit()
+            if int (month_numbered) > 12:
+                raise TypeError
+            print (year, str (month_numbered).zfill(2), str (day).zfill(2), sep = "-")
         else:
             raise TypeError
     except TypeError:
