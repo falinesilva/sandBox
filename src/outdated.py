169257aaa20ec3e1674926e74dbf043be1430014
custodf <faline.custodio.da.silva@gmail.com>
2024-11-11 08:04:45 -0300

    Updated outdated.py
    Added final features and passed tests. Did not use check50 yet.

--- src/outdated.py
+++ src/outdated.py
@@ -1,6 +1,6 @@
-# Initiate list of months
+# List of months
 
-month_list = [
+months_list = [
         "January",
         "February",
         "March",
@@ -24,12 +24,26 @@ while True:
         if "/" in user_date: # Check for / in input
             slash_date = user_date.split("/") # Split string to list
             month = slash_date[0]
+            if month > 12:
+                raise TypeError
             day = slash_date [1]
+            if day > 31:
+                raise TypeError
             year = slash_date[2]
             print (year, month.zfill(2), day.zfill(2), sep ="-") # Print formatted date with padding
+            exit()
         elif "," in user_date: # Check for , in input
-            print (",")
-            # WIP
+            comma_date = user_date.split()
+            year = comma_date[2]
+            day = comma_date [1].replace(',', '').replace('st', '').replace('rd', '').replace('nd', '').replace('th', '')
+            if day > 31:
+                raise TypeError
+            month = comma_date[0]
+            if month > 12:
+                raise TypeError
+            month_numbered = str (months_list.index(comma_date[0]) + 1)
+            print (year, month_numbered.zfill(2), day.zfill(2), sep = "-")
+            exit()
         else:
             raise TypeError
     except TypeError:
