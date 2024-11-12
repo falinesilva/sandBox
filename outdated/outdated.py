abb9580f459db2c9d76849cd25facf23987f7ea1
custodf <faline.custodio.da.silva@gmail.com>
2024-11-12 13:41:16 +0000

    Tue, Nov 12, 2024, 10:41 AM -03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -25,10 +25,10 @@ while True:
             slash_date = user_date.split("/") # Split string to list
             month = int (slash_date[0])
             if month > 12:
-                raise ValueError
+                raise TypeError
             day = int (slash_date [1])
-            if day > 31:
-                raise ValueError
+            if day < 1 or day > 31:
+                raise TypeError
             year = slash_date[2]
             print (year, str(month).zfill(2), str(day).zfill(2), sep ="-") # Print formatted date with padding
             exit()
@@ -37,11 +37,11 @@ while True:
             year = comma_date[2]
             day = int (comma_date [1].replace(',', '').replace('st', '').replace('rd', '').replace('nd', '').replace('th', ''))
             if day > 31:
-                raise ValueError
+                raise TypeError
             month = comma_date[0].strip(',')
             month = str (months_list.index(comma_date[0]) + 1)
             if int (month) <1 or int (month) > 12:
-                raise ValueError
+                raise TypeError
             print (year, str (month).zfill(2), str (day).zfill(2), sep = "-")
             exit()
         else:
