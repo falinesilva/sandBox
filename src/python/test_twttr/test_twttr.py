4565f64b081dc44c4c893f38623143ece9080da0
custodf <faline.custodio.da.silva@gmail.com>
2024-12-04 07:06:32 -0300

    Created test_twttr.py

---
+++ src/python/test_twttr/test_twttr.py
@@ -0,0 +1,8 @@
+import twttr
+from twttr import shorten
+
+def test_shorten():
+    assert shorten("HELLO") == "HLL"
+    assert shorten("hello") == "hll"
+    assert shorten("Hi 123") == "H 123"
+    assert shorten("hello, world") == "hll, wrld"
