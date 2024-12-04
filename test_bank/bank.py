6df8197d21845c7f326c88c78537122c857c4385
custodf <faline.custodio.da.silva@gmail.com>
2024-12-04 11:50:10 +0000

    Wed, Dec 4, 2024, 8:50 AM -03:00

---
+++ test_bank/bank.py
@@ -0,0 +1,28 @@
+def main():
+    # Get greeting from user
+    greeting = input("Greeting: ").lower().strip()
+
+    # Get value of the greeing
+    response = value(greeting)
+
+    # Print value of the greeting
+    print(f"${response}")
+
+
+def value(greeting):
+
+    # Return 0 if the greeting is 'hello'
+    if greeting == ("hello"):
+        return 0
+
+    # Return 100 if the greeting does not start with 'h'
+    elif not greeting.startswith("h"):
+        return 100
+
+    # Return 20 if the greeting starts with 'h' but is not 'hello'
+    else:
+        return 20
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_bank/test_bank.py
@@ -0,0 +1,7 @@
+import bank
+from bank import value
+
+def test_value():
+    assert value("what's up!") == 100
+    assert value("hello") == 0
+    assert value("hi") == 20
