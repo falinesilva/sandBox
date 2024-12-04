2594a88bed9b16a5f258a961efb90ea33de51c0c
custodf <faline.custodio.da.silva@gmail.com>
2024-12-04 10:04:42 +0000

    Wed, Dec 4, 2024, 7:04 AM -03:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -4,4 +4,5 @@ from twttr import shorten
 def test_shorten():
     assert shorten("HELLO") == "HLL"
     assert shorten("hello") == "hll"
-    assert shorten("Hi 123") == "H"
+    assert shorten("Hi 123") == "H 123"
+    assert shorten("hello, world") == "hll, wrld"
