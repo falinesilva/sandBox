1a266f7a69250faea57077874b87ddf0f512a5ad
custodf <faline.custodio.da.silva@gmail.com>
2024-12-05 06:24:47 -0300

    Created test_plates.py

---
+++ src/python/test_plates/plates.py
@@ -0,0 +1,42 @@
+def main():
+
+    plate = input("Plate: ")
+
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+
+   # Check length
+    if len(s) > 6 or len(s) < 2:
+        return False
+
+    # Check if first two characters are alphabetical
+    if not s[0:2].isalpha():
+        return False
+
+    # Check numbers in plate
+    for i, c in enumerate(s):
+        if c.isdigit():
+            # Found the first digit
+            if c == "0":
+                return False
+            # Check if all remaining characters are digits
+            elif all(char.isdigit() for char in s[i:]):
+                break
+            else:
+                return False
+
+    # Check that all characters are alphanumeric
+    for i in s:
+        if not i.isalnum():
+            return False
+
+    return True
+
+
+if __name__ == "__main__":
+    main()
