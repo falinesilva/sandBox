c631ccbc021c1b74c2ecc33b45efe9e2290b2419
custodf <faline.custodio.da.silva@gmail.com>
2024-12-06 14:48:34 -0300

    update fuel.py
    Restarted fuel.py for test_fuel.py

--- src/python/test_fuel/fuel.py
+++ src/python/test_fuel/fuel.py
@@ -1,32 +1,22 @@
 def main():
-    # Get input from the user
+
     while True:
         try:
-            user_fraction = input("Fraction: ")
+            user_input = input("Fraction: ")
+            x, y = map(int, user_input.split('/'))
+            if y == 0 or y < x:
+                raise ValueError
         except ValueError:
             pass
         except ZeroDivisionError:
             pass
-    user_fraction_converted = convert(user_fraction)
-    
-    gauge_result = gauge(user_fraction_converted)
 
-    # Print percentage
-    if percentage <= 1:
-        print("E")
-    elif percentage >= 99:
-        print("F")
-    else:
-        print(f"{int(percentage)}%")
+
 def convert(fraction):
-# Convery to fraction
-    x, y = map(int, fraction.split('/'))
-    if y == 0 or y < x:
-        raise ValueError("Denominator cannot be zero.")
+
 def gauge(percentage):
-    # Process percentage
-    percentage = (x / y) * 100
-    percentage = round(percentage)
+
+    percentage = 
 
 
 if __name__== "__main__":
