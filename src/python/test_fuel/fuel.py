719cdc7696feff339d30ef6d1e87e61ba13f2df6
custodf <faline.custodio.da.silva@gmail.com>
2024-12-09 08:13:16 -0300

    Updated new fuel.py
    Working version of fuel.py to work with test_fuel.py.

--- src/python/test_fuel/fuel.py
+++ src/python/test_fuel/fuel.py
@@ -1,36 +1,40 @@
 def main():
 
-    x = input("Fraction: ")
-    
-    fraction = convert(x)
-    print(fraction)
-
-    
-def convert(fraction):
     while True:
         try:
-            if '/' not in fraction:
-                raise ValueError
-            x, y = map(int, fraction.split('/', maxsplit=2))
-            if x > y:
-                raise ZeroDivisionError
-        
+            user_fraction = input("Fraction: ")
+            fraction = convert(user_fraction)
+            result = gauge(fraction)
+            print(result)
+            break
+        except ValueError:
+            pass
+        except ZeroDivisionError:
+            pass
+
+def convert(fraction):
+    try:
+        x, y = map(int, fraction.split('/', maxsplit=1))
+        if y ==0:
+            raise ZeroDivisionError
+        elif x > y:
+            raise ValueError
+        else: 
             percentage = int( (x / y) * 100)
             percentage = round(percentage)
-            return percentage
-        except ValueError:
-            print("ValueError")    
-            break
-        except ZeroDivisionError:
-            break
+        return percentage
+    except ValueError:
+        raise ValueError
+    except ZeroDivisionError:
+        raise ZeroDivisionError
 
-# def gauge(percentage):
-    #if percentage <= 1:
-             #       print("E")
-              #  elif percentage >= 99:
-              #      print("F")
-               # else:
-                #    print(f"{int(percentage)}%")
+def gauge(percentage):
+    if percentage <= 1:
+        return("E")
+    elif percentage >= 99:
+        return("F")
+    else:
+        return(f"{int(percentage)}%")
 
 if __name__== "__main__":
     main()
