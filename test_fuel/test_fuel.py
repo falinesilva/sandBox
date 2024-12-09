a8532cadde2bfddf27fa87f212045bbbb3518350
custodf <faline.custodio.da.silva@gmail.com>
2024-12-09 11:32:47 +0000

    Mon, Dec 9, 2024, 8:32 AM -03:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -13,6 +13,9 @@ def test_convert():
           convert("100/0")
 def test_gauge():
      assert gauge(-1) == "E"
+     assert gauge(1) == "E"
+     assert gauge(0) == "E"
      assert gauge(100) == "F"
      assert gauge(120) == "F"
      assert gauge(60) == "60%"
+     assert gauge(99) == "99%"
