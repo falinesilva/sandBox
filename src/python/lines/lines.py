2c5a2c1980070321d0abade6b0aff2311ba96569
custodf <faline.custodio.da.silva@gmail.com>
2024-12-11 09:48:17 -0300

    Finished lines.py
    Completed lines.py passes all tests.

--- src/python/lines/lines.py
+++ src/python/lines/lines.py
@@ -1,16 +1,33 @@
 import sys
 
 def main():
-    
-   user_file = get_argument(sys.argv)
-   print(user_file)
+
+    user_file = get_argument(sys.argv)
+   
+    try:
+        with open(user_file) as file:
+            lines = file.readlines()
+    except FileNotFoundError:
+        sys.exit("File does not exist")
+
+    print(filter(lines))
+
+def filter(lines):
+    filtered_lines = []
+    for line in lines:
+        line = line.lstrip()
+        if line != '':
+            if not line.startswith('#'):
+                filtered_lines.append(line)
+
+    return(len(filtered_lines))
 
 def get_argument(command_line):
     
     if len(command_line) > 2:
-        sys.exit("Too many arguments")
+        sys.exit("Too many command-line arguments")
     elif len(command_line) < 2:
-        sys.exit("Too few arguments")
+        sys.exit("Too few command-line arguments")
     elif not command_line[1].endswith('.py'):
         sys.exit("Not a python file")
     else:
---
+++ src/python/lines/test.py
@@ -0,0 +1,10 @@
+a = 'hello, world'
+b = 1
+print(a)
+
+for _ in list:
+    print('hello')
+
+#
+
+__name__
