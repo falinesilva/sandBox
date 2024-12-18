817d26debcc5a683e485f0476ede12413c8b0a75
custodf <faline.custodio.da.silva@gmail.com>
2024-12-17 22:55:09 -0300

    Update numb3rs.py
    Working so far to validate IP's.

--- src/python/numb3rs/numb3rs.py
+++ src/python/numb3rs/numb3rs.py
@@ -6,9 +6,10 @@ def main():
     print(validate(input("IPv4 Address: ")))
 
 
-
 def validate(ip):
-    result = re.search(r"[0-9]\.",ip)
+    result = re.search(r"^([0-9]+[10-99]*[100-255]*\.){3}[0-9]+[10-99]*[100-255]*$",ip)
+    if result:
+        print (result[1])
     if result:
         return('True')
     else:
