23b97338619a945ac6095d809e49b8edfcdd7857
custodf <faline.custodio.da.silva@gmail.com>
2024-12-19 02:19:12 +0000

    Wed, Dec 18, 2024, 11:19 PM -03:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -1,9 +1,5 @@
 from numb3rs import validate
 
-def main():
-    test_validate_true()
-    test_validate_false()
-
 def test_validate_true():
     assert validate('1.2.3.4') == 'True'
     assert validate('255.255.255.255') == 'True'
@@ -14,6 +10,3 @@ def test_validate_false():
     assert validate('cat') == 'False'
     assert validate('256.255.255.255') == 'False'
     assert validate('111.111.111.111111111') == 'False'
-
-if __name__ == "__main__":
-    main()
