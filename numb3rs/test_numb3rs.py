ba1df1d9957bc7628e6f137529f5d7b18216fcf7
custodf <faline.custodio.da.silva@gmail.com>
2024-12-19 02:23:06 +0000

    Wed, Dec 18, 2024, 11:23 PM -03:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -1,12 +1,12 @@
 from numb3rs import validate
 
 def test_validate_true():
-    assert validate('1.2.3.4') == 'True'
-    assert validate('255.255.255.255') == 'True'
-    assert validate('127.0.0.1') == 'True'
+    assert validate('1.2.3.4') == True
+    assert validate('255.255.255.255') == True
+    assert validate('127.0.0.1') == True
 
 
 def test_validate_false():
-    assert validate('cat') == 'False'
-    assert validate('256.255.255.255') == 'False'
-    assert validate('111.111.111.111111111') == 'False'
+    assert validate('cat') == False
+    assert validate('256.255.255.255') == False
+    assert validate('111.111.111.111111111') == False
