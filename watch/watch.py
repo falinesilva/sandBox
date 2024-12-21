e8a15e66798d7fa1498e4dc3e52621b1de1ec487
custodf <faline.custodio.da.silva@gmail.com>
2024-12-21 15:43:39 +0000

    Sat, Dec 21, 2024, 12:43 PM -03:00

---
+++ watch/watch.py
@@ -0,0 +1,17 @@
+import re
+import sys
+
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    html = re.search(r".*\"http.*(youtube.*) title=.*", s)
+    if html:
+        link = str(html[1])
+        updated_link = link.replace('"', '').replace('youtube.com/embed/', 'youtu.be/').replace('', '')
+        return(updated_link)
+
+if __name__ == "__main__":
+    main()
