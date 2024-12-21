c52abe825cc902bbf6a6b409df19adc4ddb3f397
custodf <faline.custodio.da.silva@gmail.com>
2024-12-21 19:27:08 -0300

    Update watch.py
    Completed. Passes all tests.

--- src/python/watch/watch.py
+++ src/python/watch/watch.py
@@ -7,10 +7,14 @@ def main():
 
 
 def parse(s):
-    html = re.search(r"<iframe.*(http.*youtube\.com/embed/.*\").*</iframe>$", s)
+    html = re.search(r"<iframe.*(http.*\.*youtube\.com/embed/.*\").*</iframe>$", s)
     if html:
         link = str(html[1])
         updated_link = link.replace('"', '').replace('youtube.com/embed/', 'youtu.be/').replace('', '')
+        if 'http:' in updated_link:
+            updated_link = updated_link.replace('http:', 'https:')
+        if 'www.' in updated_link:
+            updated_link = updated_link.replace('www.', '')
         return(updated_link)
 
 if __name__ == "__main__":
