9c68c212a93a1be88daee47d6b8de2fa65365219
custodf <faline.custodio.da.silva@gmail.com>
2024-12-23 03:37:34 -0300

    Update working.py

--- src/python/working/working.py
+++ src/python/working/working.py
@@ -7,12 +7,19 @@ def main():
 
 
 def convert(s):
-    match = re.search(r"^(\d)(.*|:\d\d)(AM|PM) to .*", s)
-    if match:
-        print(match[1]) # Start hours
-        print(match[2])# Start minutes
-        print(match[3]) # Start AM|PM
-
+    try:
+        match = re.search(r"^(\d)(.*|:\d\d)(AM|PM) to (\d)(.*|:\d\d)(AM|PM)$", s)
+        if match:
+            print(match[1]) # Start hours
+            print(match[2]) # Start minutes
+            print(match[3]) # Start AM|PM
+            print(match[4]) # Finish hours
+            print(match[5]) # Finish minutes
+            print(match[6]) # Finish AM|PM
+        if not match:
+            raise ValueError
+    except ValueError:
+        return ('ValueError')
 
 if __name__ == "__main__":
     main()
