f5659f6eec6b3bb2a22f5291c0d05d9e75240639
custodf <faline.custodio.da.silva@gmail.com>
2024-12-23 20:49:02 -0300

    Update working.py
    Still need to sort out 12AM.

--- src/python/working/working.py
+++ src/python/working/working.py
@@ -9,29 +9,33 @@ def main():
 def convert(s):
     try:
         match = re.search(r"^(\d)(.*|:\d\d)(AM|PM) to (\d)(.*|:\d\d)(AM|PM)$", s)
-        if match:
-            print(match[1]) # Start hours
-            print(match[2]) # Start minutes
-            print(match[3]) # Start AM|PM
-            print(match[4]) # Finish hours
-            print(match[5]) # Finish minutes
-            print(match[6]) # Finish AM|PM
+
         if not match:
             raise ValueError
         
         if 'PM' in match[3]:
-            print('Starts in PM')
             start_hours = (int(match[1]) + 12)
         else:
-            start_hours = (f"{match[1]:02}")
+            start_hours = f"{int(match[1]):02}"
         if 'PM' in match [6]:
-            print('Ends in PM')
             end_hours = (int(match[4]) + 12)
         else:
-            end_hours = (f"{match[4]:02}")
+            end_hours = f"{int(match[4]):02})"
         
-        hours = (f"{start_hours}{match[2]}to {end_hours}{match[5]}")
+        if match[2] == ' ':
+            start_minutes = (':00')
+        else:
+            start_minutes = match[2].strip()
+
+        if match[5] == ' ':
+            finish_minutes = (':00')
+        else:
+            finish_minutes = match[5].strip()
+
+        hours = (f"{start_hours}{start_minutes} to {end_hours}{finish_minutes}")
         return hours
+    
+    
     except ValueError:
         return ('ValueError')
 
