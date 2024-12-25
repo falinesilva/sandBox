8cbe1291175d839886f87e7bbc3a74ea9fc55a6b
custodf <faline.custodio.da.silva@gmail.com>
2024-12-25 01:12:18 -0300

    Update working.py and test_working.py
    Passing all initial tests.

--- src/python/working/test_working.py
+++ src/python/working/test_working.py
@@ -15,4 +15,4 @@ def test_convert_b(): # AM/PM format tests
 def test_convert_c(): # Hour and minute range tests
     assert convert('9:60 AM to 5:59 PM') == 'ValueError'
     assert convert('13:00 PM to 13:00 AM') == 'ValueError'
-    #assert convert('12:00 AM to 12:00 PM') == '00:00 to 12:00'
+    assert convert('12:00 AM to 5:59 PM') == '00:00 to 17:59'
