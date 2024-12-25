4c66fb4fe000ea9af163c464fac28914620a8952
custodf <faline.custodio.da.silva@gmail.com>
2024-12-25 18:30:06 -0300

    Update working.py and test_working.py
    Adding tests and fixing issue with 12PM and 12AM.

--- src/python/working/test_working.py
+++ src/python/working/test_working.py
@@ -5,6 +5,8 @@ def test_convert_a(): # Hour and minute conversion tests
     assert convert('9:00 AM to 5:00 PM') == '09:00 to 17:00'
     assert convert('10 AM to 8:50 PM') == '10:00 to 20:50'
     assert convert('10:30 PM to 8:00 AM') == '22:30 to 08:00'
+    assert convert('12:00 AM to 12:00 PM') == '00:00 to 12:00'
+    assert convert('12 AM to 12 PM') == '00:00 to 12:00'
 
 def test_convert_b(): # AM/PM format tests
     assert convert('9:30 to 5:30') == 'ValueError'
--- src/python/working/working.py
+++ src/python/working/working.py
@@ -10,36 +10,46 @@ def convert(s):
     try:
         match = re.search(r"^(\d{1,2})(.*|:\d\d)(AM|PM) to (\d)(.*|:\d\d)(AM|PM)$", s)
 
+        # Check for match
         if not match:
             raise ValueError
 
+        # Check both hours
         if int(match[1]) > 12 or int(match[4]) > 12:
             raise ValueError
+        
+        # Check start time hours
         if 'PM' in match[3]:
             start_hours = (int(match[1]) + 12)
         elif 'AM' in match[3] and int(match[1]) == 12:
             start_hours = f"{00:02}"
         else:
             start_hours = f"{int(match[1]):02}"
-        if 'PM' in match [6]:
-            end_hours = (int(match[4]) + 12)
-        else:
-            end_hours = f"{int(match[4]):02}"
         
+        # Check start time minutes
         if not ':' in match[2]:
             start_minutes = '00'
         else:
             start_minutes = match[2].strip().replace(':', '')
 
+        # Check finish time hours
+        if 'PM' in match [6]:
+            finish_hours = (int(match[4]) + 12)
+        else:
+            finish_hours = f"{int(match[4]):02}"
+        
+        # Check finish time minutes
         if not ':' in match[5]:
             finish_minutes = '00'
         else:
             finish_minutes = match[5].strip().replace(':', '')
         
+        # Check both minutes
         if int(start_minutes) > 59 or int(finish_minutes) > 59:
             raise ValueError
         
-        hours = (f"{start_hours}:{start_minutes} to {end_hours}:{finish_minutes}")
+        # Format final output
+        hours = (f"{start_hours}:{start_minutes} to {finish_hours}:{finish_minutes}")
         return hours
     
     except ValueError:
