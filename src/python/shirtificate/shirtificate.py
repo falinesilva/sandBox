3bc16bb55885596b8e9ba5c46afc1cd0dc9e1859
custodf <faline.custodio.da.silva@gmail.com>
2025-01-13 16:52:11 -0300

    Competed shirtificate.py
    Passes all checks.

--- src/python/shirtificate/shirtificate.py
+++ src/python/shirtificate/shirtificate.py
@@ -1,7 +1,14 @@
 from fpdf import FPDF
 
-pdf = FPDF()
+name = input("Name: ")
+
+pdf = FPDF(orientation="portrait", unit="mm", format="A4")
 pdf.add_page()
-pdf.set_font('helvetica', size=12)
-pdf.cell(text="Faline took CS50")
+pdf.set_font('helvetica', size=25)
+pdf.image("shirtificate.png", w=180, x=15, y=58.5)
+text = f"{name} took CS50"
+pdf.set_xy(0, 110)
+pdf.set_text_color(255, 255, 255)
+pdf.cell(pdf.w, 10, text, align="C")
+
 pdf.output("shirtificate.pdf")
