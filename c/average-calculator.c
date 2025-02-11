4628995d3b8aead83fc892af2aea0f4a39fa483b
custodf <faline.custodio.da.silva@gmail.com>
2025-02-11 14:07:44 -0300

    Renamed average-calculator.c Fixed issue with average-calculator.c equation

--- c/average-calculator.c
+++ c/average-calculator.c
@@ -1,21 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
-// Average calculation
-const int N = 3;
+const int N = 3; //TODO magic number
 
-// Prototype
 float average(int length, int array[]);
 
 int main(void)
 {
-    // Get scores
     int scores[N];
     for (int i = 0; i < N; i++)
     {
         scores [i] = get_int("Score: ");
     }
-    // Print average
-    printf("Average: %f", (scores[0] + scores [1] + scores [2]) / N);
+    printf("Average: %.2f", (scores[0] + scores [1] + scores [2]) / (float)N);
 
 }
