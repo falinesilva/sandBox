9471a6e975dd39979a30e13d170d011bfbf30b89
custodf <faline.custodio.da.silva@gmail.com>
2025-02-11 12:24:01 -0300

    Organizing directories in /c and renamed cipher.c

--- c/cipher.c
+++ c/cipher.c
@@ -1,34 +1,29 @@
+//Create a cipher text with command-line input.
+
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// Verify key is a single command.
 bool check_key(int argc, string arg);
 
-// Encipher a plain text input with the key.
 string encipher(string plaintext, int key);
 
 int main(int argc, string argv[])
 {
-    // Get key and verify.
     if (!check_key(argc, argv[1]))
     {
         printf("Usage: ./caesar key");
         return 1;
     }
 
-    // Convert key to integer.
     int key = atoi(argv[1]);
 
-    // Ask user to input plaintext.
     string plain = get_string("Enter plaintext: \n");
 
-    // Encipher plaintext.
     plain = encipher(plain, key);
 
-    // Print ciphertext.
     printf("ciphertext: %s\n", plain);
 }
 
@@ -37,7 +32,6 @@ string encipher(string plaintext, int secret)
     int n = strlen(plaintext);
     for (int i = 0; i < n; i++)
     {
-        // Encrypt alphabetical characters using the Caesar cipher formula.
         if (isalpha(plaintext[i]))
         {
             if (islower(plaintext[i]))
@@ -55,11 +49,9 @@ string encipher(string plaintext, int secret)
 
 bool check_key(int argc, string arg)
 {
-    // Verify key is a single argument.
     if (argc != 2)
         return false;
 
-    // Verify key is a valid non-negative integer.
     for (int i = 0; arg[i] != '\0'; i++)
     {
         if (!isdigit(arg[i]))
