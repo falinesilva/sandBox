82079a071e4cfec55cbaa04cdee64e903a9ced06
custodf <faline.custodio.da.silva@gmail.com>
2025-02-12 01:12:05 -0300

    Renamed list-search.c to car.c Added a tolower function to car.c Changed list and print statements in car.c

--- c/car/car.c
+++ c/car/car.c
@@ -3,22 +3,32 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
+void makelowercase(string s)
+{
+    for (int i = 0; i < strlen(s); i++)
+        {
+            s[i] = tolower(s[i]);
+        }
+    }
 int main(void)
 {
     printf("Hey! What's your favorite car brand?\n");
     
     string strings[] = {"honda", "nissan", "toyota", "suzuki", "subaru", "mitsubishi"};
 
-    string s = get_string("\nBrand: ");
+    string brand = get_string("Brand: ");
+    makelowercase(brand);
     for (int i = 0; i < 6; i++) //TODO: Magic number
     {
-        if (strcmp(strings[i], s) == 0)
+        if (strcmp(strings[i], brand) == 0)
         {
             printf("Nice choice! Japanese is the best.\n");
             return 0;
         }
     }
+
     printf("Hmm...\n");
     return 1;
 }
