3da6c4f86c92f9c5d95b31ce2bd20ea460f68584
custodf <faline.custodio.da.silva@gmail.com>
2025-02-12 00:10:03 -0300

    Deleted greet.c because it was useless Moved name.c to name/

---
+++ c/name/name.c
@@ -0,0 +1,11 @@
+// Input your name and print a greeting
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+
+{
+    string answer = get_string("What's your name? ");
+    printf("Hello, %s!\n", answer);
+}
