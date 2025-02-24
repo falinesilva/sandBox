9318ccd807dc753f3538886b074ff2043495b044
custodf <faline.custodio.da.silva@gmail.com>
2025-02-24 12:22:02 -0300

    Working on camping.js

--- js/camping/camping.js
+++ js/camping/camping.js
@@ -1,3 +1,16 @@
 // Make a pyramid with JavaScript
 
-console.log("Build a pyramid\nEnter a size")
+const character = "#";
+const count = 8;
+const rows = [];
+let result = "";
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1));
+}
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+console.log(result);
