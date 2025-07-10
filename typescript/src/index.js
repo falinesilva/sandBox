3976fe3b6bca765614c8db929717e0fe1ce2b5b1
unknown <faline.silva@proton.me>
2025-07-10 06:58:46 -0300

    added more to TypeScript exercises

--- typescript/src/index.js
+++ typescript/src/index.js
@@ -1,4 +1,42 @@
+// TypeScript Exercises
+// Primitives without inference
 var cat = "Nina is my cute cat.";
+cat.toLocaleLowerCase();
 var value = 1;
 var isAnimals = true;
-document.getElementsByTagName("p")[0].innerText = cat;
+console.log(cat);
+// Primitives with inference
+var car = "Toyota Supra";
+var year = 1990;
+var isFast = true;
+console.log(car);
+// Functions with defaults
+function sum(a, b) {
+    if (b === void 0) { b = 5; }
+    return a + b;
+}
+var result = sum(5, 100);
+console.log("Result", result);
+// Functions with optionals parameters
+function fuelUsed(tank, fuel, nitro) {
+    var remainingFuel = tank - (tank * fuel) / 100;
+    if (nitro) {
+        remainingFuel += 1000;
+    }
+    return remainingFuel;
+}
+var myTripFuel = fuelUsed(100, 40, true);
+console.log("Fuel Used:", myTripFuel);
+// Object type defined inline a function parameter
+function printColors(colors) {
+    console.log("Primary", colors.primary);
+    console.log("Secondary", colors.secondary);
+}
+// Inline object type with optional chaining on parameter property
+var house = { size: "Single family", condition: "Fair" };
+function displayHouse(house) {
+    var _a;
+    console.log("House Size:", house.size, "House Condition:", (_a = house.condition) === null || _a === void 0 ? void 0 : _a.toLocaleLowerCase());
+}
+displayHouse(house);
+// Arrays
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,7 +1,71 @@
+// TypeScript Exercises
+
+// Primitives without inference
+
 let cat: string = "Nina is my cute cat.";
+
+cat.toLocaleLowerCase();
 
 let value: number = 1;
 
 let isAnimals: boolean = true;
 
-document.getElementsByTagName("p")[0].innerText = cat;
+console.log(cat);
+
+// Primitives with inference
+
+let car = "Toyota Supra";
+
+let year = 1990;
+
+let isFast = true;
+
+console.log(car);
+
+// Functions with defaults
+
+function sum(a: number, b: number = 5): number {
+  return a + b;
+}
+
+let result = sum(5, 100);
+
+console.log("Result", result);
+
+// Functions with optionals parameters
+
+function fuelUsed(tank: number, fuel: number, nitro?: boolean): number {
+  let remainingFuel = tank - (tank * fuel) / 100;
+  if (nitro) {
+    remainingFuel += 1000;
+  }
+  return remainingFuel;
+}
+
+let myTripFuel = fuelUsed(100, 40, true);
+
+console.log("Fuel Used:", myTripFuel);
+
+// Object type defined inline a function parameter
+
+function printColors(colors: { primary: string; secondary: string }) {
+  console.log("Primary", colors.primary);
+  console.log("Secondary", colors.secondary);
+}
+
+// Inline object type with optional chaining on parameter property
+
+const house = { size: "Single family", condition: "Fair" };
+
+function displayHouse(house: { size: string; condition?: string }) {
+  console.log(
+    "House Size:",
+    house.size,
+    "House Condition:",
+    house.condition?.toLocaleLowerCase()
+  );
+}
+
+displayHouse(house);
+
+// Arrays
