be7277ad266fb9292c02ae446ef54b0a01c8acf2
unknown <faline.silva@proton.me>
2025-07-10 05:43:29 -0300

    started TypeScript exercises

---
+++ typescript/src/index.js
@@ -0,0 +1,4 @@
+var cat = "Nina is my cute cat.";
+var value = 1;
+var isAnimals = true;
+document.getElementsByTagName("p")[0].innerText = cat;
---
+++ typescript/src/index.ts
@@ -0,0 +1,7 @@
+let cat: string = "Nina is my cute cat.";
+
+let value: number = 1;
+
+let isAnimals: boolean = true;
+
+document.getElementsByTagName("p")[0].innerText = cat;
